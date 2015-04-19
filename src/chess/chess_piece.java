edfd5ba4174983b11c262fef344d904f0ea1b7f2
Jinnie <yijkan@bergen.org>
2015-04-19 02:12:05 -0400

    shortened variable name

--- src/chess/chess_piece.java
+++ src/chess/chess_piece.java
@@ -2,19 +2,19 @@ package chess;
 
 public class chess_piece {
 	private String piece_type;
-	private boolean made_at_least_one_move;
+	private boolean moved;
 	private int color; // 0 for white, 1 for black
 	
 	public chess_piece(String piece_type, int color) {
 		this.piece_type=piece_type;
 		this.color=color;
-		made_at_least_one_move=false;
+		moved=false;
 	}
 	public String get_piece_type() {
 		return piece_type;
 	}
-	public boolean get_made_at_least_one_move() {
-		return made_at_least_one_move;
+	public boolean get_moved() {
+		return moved;
 	}
 	public int get_color() {
 		return color;
@@ -22,8 +22,8 @@ public class chess_piece {
 	public void set_piece_type(String piece_type) {
 		this.piece_type=piece_type;
 	}
-	public void set_made_at_least_one_move(boolean made_at_least_one_move) {
-		this.made_at_least_one_move=made_at_least_one_move;
+	public void set_moved(boolean moved) {
+		this.moved = moved;
 	}
 	public void set_color(){
 		this.color=color;
